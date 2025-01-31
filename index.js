a1bcefd6a1bdd405d3235121b2d64ab0569e1cc5
Zuwaynib <zainabadeoje@gmail.com>
2025-01-31 22:34:26 +0100

    fixed textInput Variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function soundOn() {
     let textInput = document.querySelector("#text-input").value;
 
     if (textInput.trim() !== "") { 
-        let utterance = new SpeechSynthesisUtterance(text);
+        let utterance = new SpeechSynthesisUtterance(textInput);
         speechSynthesis.speak(utterance);
     } else {
         console.log("No text entered!");
